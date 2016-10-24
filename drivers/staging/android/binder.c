e810a1c2fa5511517ad8d8c3bf659b4a44fb7afc
Arve Hjønnevåg <arve@android.com>
2016-10-24 15:20:29 +0200

    ANDROID: binder: Add strong ref checks
    commit 0a3ffab93fe52530602fe47cd74802cffdb19c05 upstream.
    
    Prevent using a binder_ref with only weak references where a strong
    reference is required.
    
    Signed-off-by: Arve Hjønnevåg <arve@android.com>
    Signed-off-by: Martijn Coenen <maco@android.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -2021,8 +2021,8 @@ static int binder_dec_node(struct binder_node *node, int strong, int internal)
 	return 0;
 }
 
-static struct binder_ref *binder_get_ref(struct binder_proc *proc, uint32_t desc,
-                                         bool need_strong_ref)
+static struct binder_ref *binder_get_ref(struct binder_proc *proc,
+					 u32 desc, bool need_strong_ref)
 {
 	struct rb_node *n = proc->refs_by_desc.rb_node;
 	struct binder_ref *ref;
@@ -2302,9 +2302,11 @@ static void binder_transaction_buffer_release(struct binder_proc *proc,
 			}
 			break;
 		case BINDER_TYPE_HANDLE:
-		case BINDER_TYPE_WEAK_HANDLE:{
-				struct binder_ref *ref = binder_get_ref(proc, fp->handle,
-                                                        fp->type == BINDER_TYPE_HANDLE);
+		case BINDER_TYPE_WEAK_HANDLE: {
+			struct binder_ref *ref;
+
+			ref = binder_get_ref(proc, fp->handle,
+					     fp->type == BINDER_TYPE_HANDLE);
 
 				if (ref == NULL) {
 					pr_err
@@ -2876,9 +2878,10 @@ out_err:
 			break;
 		case BINDER_TYPE_HANDLE:
 		case BINDER_TYPE_WEAK_HANDLE:{
-				struct binder_ref *ref = binder_get_ref(proc, fp->handle,
-                                                        fp->type == BINDER_TYPE_HANDLE);
-
+				struct binder_ref *ref;
+				
+				ref = binder_get_ref(proc, fp->handle,
+					                 fp->type == BINDER_TYPE_HANDLE);
 				if (ref == NULL) {
 					binder_user_error
 					    ("%d:%d got transaction with invalid handle, %d\n",
